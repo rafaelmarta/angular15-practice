15f0f950de427e7e7c1306bc3adb650ad86d6125
Rafael Marta <rafaelsilvamarta97@gmail.com>
2025-04-18 18:27:39 -0300

    [Users filters] Finished users filters

--- src/app/features/users/users.module.ts
+++ src/app/features/users/users.module.ts
@@ -5,7 +5,7 @@ import { UsersPageComponent } from './users-page/users-page.component';
 import { UsersFiltersComponent } from './users-filters/users-filters.component';
 import { UsersTableComponent } from './users-table/users-table.component';
 import { UsersModalComponent } from './users-modal/users-modal.component';
-import { NbLayoutModule, NbCardModule, NbInputModule, NbFormFieldModule, NbOptionModule, NbSelectModule, NbIconModule, NbButtonModule, NbContextMenuModule, NbMenuModule, NbDialogModule, NbRadioModule } from '@nebular/theme';
+import { NbLayoutModule, NbCardModule, NbInputModule, NbFormFieldModule, NbOptionModule, NbSelectModule, NbIconModule, NbButtonModule, NbContextMenuModule, NbMenuModule, NbDialogModule, NbRadioModule, NbSpinnerModule } from '@nebular/theme';
 import { NbEvaIconsModule } from '@nebular/eva-icons';
 import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 import { ReactiveFormsModule } from '@angular/forms';
@@ -35,7 +35,8 @@ import { ReactiveFormsModule } from '@angular/forms';
     NbMenuModule,
     NbDialogModule,
     NbRadioModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    NbSpinnerModule
   ]
 })
 export class UsersModule { }
